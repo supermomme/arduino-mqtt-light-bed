f1cc9aad49f1f058b1b431594bf99d4853de8570
Momme Jürgensen <momme@juergensen.me>
2019-10-18 15:37:33 +0200

    add mqtt cred

--- index.js
+++ index.js
@@ -1,52 +1,11 @@
 var mqtt = require('mqtt')
+var pixel = require("node-pixel")
+var firmata = require('firmata')
 
-var board = new five.Board()
-var strip = null
-
-board.on("ready", function() {
-  strip = new pixel.Strip({
-    board: this,
-    controller: "FIRMATA",
-    strips: [
-      {pin: 6, length: 60},
-      {pin: 7, length: 60}
-    ],
-    gamma: 2.8
-  })
-
-  strip.on("ready", function() {
-    var SEQUENZ = "FULL_RED"
-    console.log("Strip ready connecting to mqtt...")
-    var client  = mqtt.connect('mqtt://10.0.100.10')
-    client.on('connect', function () {
-      console.log("Connected")
-      client.subscribe(['home/room/momme/light/bed', 'home/room/momme/light/bed/#'], function (err) {
-        if (!err) {
-          client.publish('home/room/momme/light/bed', { val: "FULL_GREEN", connected: true })
-        }
-      })
-    })
-
-    client.on('message', function (topic, message) {
-      if ()
-      console.log(message.toString())
-      client.end()
-    })
-
-    setInterval(() => {
-      if (SEQUENZ === "FULL_RED") {
-        strip
-      }
-    }, 1000/20);
-
-  })
-})
-
-
-pixel = require("node-pixel");
-var firmata = require('firmata');
-
-const fps = 20;
+const fps = 20
+const hostname = process.env.MQTT_HOSTNAME
+const password = process.env.MQTT_PASSWORD
+const username = process.env.MQTT_USERNAME
 
 var board = new firmata.Board('/dev/ttyACM0',function(){
   strip = new pixel.Strip({
@@ -62,7 +21,11 @@ var board = new firmata.Board('/dev/ttyACM0',function(){
     var SEQUENZ = "FULL_RED"
     console.log("Strip ready connecting to mqtt...")
 
-    var client  = mqtt.connect('mqtt://10.0.100.10')
+    var client  = mqtt.connect(`mqtt://${hostname}`, {
+      clientId: 'mommes-bett',
+      username,
+      password
+    })
     client.on('connect', function () {
       console.log("Connected")
       client.subscribe(['home/room/momme/light/bed', 'home/room/momme/light/bed/#'], function (err) {
@@ -90,6 +53,6 @@ var board = new firmata.Board('/dev/ttyACM0',function(){
       }
       strip.show()
     }, 1000/fps)
-  });
-});
+  })
+})
 
