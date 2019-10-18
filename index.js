565f4c2af222490b1e1a6f2a029e5d78437b9c6f
Momme Jürgensen <momme@juergensen.me>
2019-10-18 11:58:52 +0200

    blind prog

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-pixel = require("node-pixel");
-five = require("johnny-five");
+pixel = require("node-pixel")
+five = require("johnny-five")
+var mqtt = require('mqtt')
 
-var board = new five.Board();
-var strip = null;
+var board = new five.Board()
+var strip = null
 
 board.on("ready", function() {
   strip = new pixel.Strip({
@@ -13,9 +14,33 @@ board.on("ready", function() {
       {pin: 7, length: 60}
     ],
     gamma: 2.8
-  });
+  })
 
   strip.on("ready", function() {
-    // do stuff with the strip here.
-  });
-});
+    var SEQUENZ = "FULL_RED"
+    console.log("Strip ready connecting to mqtt...")
+    
+    var client  = mqtt.connect('mqtt://10.0.100.10')
+    client.on('connect', function () {
+      console.log("Connected")
+      client.subscribe(['home/room/momme/light/bed', 'home/room/momme/light/bed/#'], function (err) {
+        if (!err) {
+          client.publish('home/room/momme/light/bed', { val: "FULL_GREEN", connected: true })
+        }
+      })
+    })
+     
+    client.on('message', function (topic, message) {
+      if ()
+      console.log(message.toString())
+      client.end()
+    })
+
+    setInterval(() => {
+      if (SEQUENZ === "FULL_RED") {
+        strip
+      }
+    }, 1000/20);
+
+  })
+})
